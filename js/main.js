9b784f48df039af31025bd34c2598fd0acf48a2a
Daniel Martin <51744227+DanielMartin971@users.noreply.github.com>
2023-03-09 07:40:33 -0800

    changed the tooltips and working on a overhaul for the main page when selecting character

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 // Flasks are the healing system in the game
 let flask = {
